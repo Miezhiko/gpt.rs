7dc1c5ab2661cd5d9e16e87d5c139c97e785bc37
Miezhiko <Miezhiko@gmail.com>
2024-09-02 11:20:17 +0400

    fix some model key
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- chat/src/g4f/blackbox.rs
+++ chat/src/g4f/blackbox.rs
@@ -73,7 +73,7 @@ impl Generator for BlackboxGenerator {
               messages.append({"role": "assistant", "content": tup[1]})
         try:
           messages.append({"role": "user", "content": prompt})
-          rspns = g4f.ChatCompletion.create( model="gpt-3.5-turbo", messages=messages
+          rspns = g4f.ChatCompletion.create( model="blackbox", messages=messages
                                            , stream=False, auth="jwt"
                                            , provider=g4f.Provider.Blackbox )
           if not rspns:
